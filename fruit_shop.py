3a6e9c4b9de35e8bd4b13658cf02ccc13be1c0f7
William <st17024@howick.school.nz>
2021-05-11 11:06:40 +1200

    commit

--- fruit_shop.py
+++ fruit_shop.py
@@ -1,5 +1,6 @@
 #from tkinter import *
 import tkinter as tk
+from tkinter import ttk
 import random
 from test import fruit
 
@@ -7,34 +8,49 @@ class Program(tk.Frame):
     def __init__(self, master=None):
         super().__init__(master)
         self.master = master
-        self.pack()
+        self.grid()
+        self.a = self.c()
         self.create_widgets()
 
-    def window(self):
-        a=("x")
 
     def create_widgets(self):
-        self.hi_there = tk.Button(self)
-        self.hi_there["text"] = "Hello World\n(click me)"
-        self.hi_there["command"] = self.say_hi
-        self.hi_there.pack(side="top")
 
+        n = tk.StringVar()
+        abc = ttk.Combobox(self, width = 27, textvariable = n)
+        abc["values"] = (self.a)
+        abc.grid(column=5, row=1)
+        abc.current()
+
+        
+        # self.hi_there = tk.Button(self)
+        # self.hi_there["text"] = "Hello World\n(click me)"
+        # self.hi_there["command"] = self.say_hi
+        # self.hi_there.grid(column=5, row=2)
+
+        xd = tk.Button(self,width="5", text="+1")
+        xd.grid(column=5, row=2)
 
         self.quit = tk.Button(self, text="QUIT", fg="red",
                               command=self.master.destroy)
-        self.quit.pack(side="bottom")
+        self.quit.grid(column=5, row=3)
 
-        clicked = tk.StringVar()
-        clicked.set("Jan")
-        Opt = ["Jan","Feb", "Mar"]
-        self.drop_down = tk.OptionMenu(self,clicked,*Opt)
 
-        self.drop_down.pack()
+        acb = tk.Label(self, text="", width="80", height="20")
+        acb.grid(column=5, row=4)
+
+
     
     def say_hi(self):
         print("hi there, everyone!")
         for i in fruit:
 	        print(fruit[i][FRUIT_NAME],fruit[i][FRUIT_COST])
+
+    def c(self):
+        a = []
+        for i in fruit:
+            x = fruit[i][FRUIT_NAME]
+            a.append(x)
+        return(a)
 
 FRUIT_NAME = "Name"
 FRUIT_COST = "Price"
--- test.py
+++ test.py
@@ -5,7 +5,20 @@ fruit = {
 	'exotic2' : {"Name" : "Durian",
 				"Price" : "$4.50 each"
 				},
+	'exotic3': {"Name" : "Guava", 
+				"Price" : "$2 each"
+				},
 	'normal1': {"Name" : "Banana", 
-				"Price" : "1.50 each"
-				}
+				"Price" : "$1.50 each"
+				},
+	'normal2': {"Name" : "Orange (6pck)", 
+				"Price" : "$5 each"
+				},
+	'normal3': {"Name" : "Kiwi(4pck)", 
+				"Price" : "$2.5 each"
+				},
+	'normal4': {"Name" : "Strawberry(150g)", 
+				"Price" : "$3 each"
+				},
+	
 }

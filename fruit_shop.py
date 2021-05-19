0834cd9573f9084b819b2c75622452255bab3f00
William <st17024@howick.school.nz>
2021-05-19 14:47:19 +1200

    commit

--- fruit_shop.py
+++ fruit_shop.py
@@ -3,6 +3,8 @@ import tkinter as tk
 from tkinter import ttk
 import random
 from test import fruit
+from tkinter import messagebox
+from PIL import ImageTk, Image  
 
 class Program(tk.Frame):
     def __init__(self, master=None):
@@ -16,18 +18,18 @@ class Program(tk.Frame):
     def create_widgets(self):
 
         n = tk.StringVar()
-        abc = ttk.Combobox(self, width = 27, textvariable = n)
-        abc["values"] = (self.a)
-        abc.grid(column=5, row=1)
-        abc.current()
+        self.abc = ttk.Combobox(self, width = 27, textvariable = n)
+        self.abc["values"] = (self.a)
+        self.abc.grid(column=5, row=1)
+        self.abc.current(0)
 
         
         # self.hi_there = tk.Button(self)
         # self.hi_there["text"] = "Hello World\n(click me)"
         # self.hi_there["command"] = self.say_hi
-        # self.hi_there.grid(column=5, row=2)
+        # self.hi_there.grid(column=5, row=2) 
 
-        xd = tk.Button(self,width="5", text="+1")
+        xd = tk.Button(self,width="5", text="+1", command=self.mt)
         xd.grid(column=5, row=2)
 
         self.quit = tk.Button(self, text="QUIT", fg="red",
@@ -38,7 +40,16 @@ class Program(tk.Frame):
         acb = tk.Label(self, text="", width="80", height="20")
         acb.grid(column=5, row=4)
 
+        xyz = ttk.Button(self, text="Get Value",command=self.check)
+        xyz.grid(column=5, row=4)
 
+    
+    def check(self):
+        a = self.abc.get()
+        print(a)
+
+    def mt(self):
+        print(amount)
     
     def say_hi(self):
         print("hi there, everyone!")
@@ -52,8 +63,12 @@ class Program(tk.Frame):
             a.append(x)
         return(a)
 
+        
+
 FRUIT_NAME = "Name"
 FRUIT_COST = "Price"
+image1 = Image.open("temp.png")
+amount = 0
 root = tk.Tk()
 app = Program(master=root)
 app.mainloop()
